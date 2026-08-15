60c4fa0298894d263b9caf2622ba386094353d27
Shivam Sharma <shivamcodesss@gmail.com>
2026-08-15 21:52:32 +0530

    User schema added

---
+++ src/model/User.js
@@ -0,0 +1,16 @@
+import mongoose, { Schema,model } from "mongoose";
+
+const UserSchema=new Schema({
+    email:{
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type: String,
+        required: true,
+    }
+},{timestamps: true})
+
+const User=mongoose.models.User || model("User",UserSchema);
+export default User;
