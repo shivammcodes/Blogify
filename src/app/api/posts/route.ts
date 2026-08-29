9bf709493c2cc8aae5bafd27f4358baeb55e1111
Shivam Sharma <shivamcodesss@gmail.com>
2026-08-29 07:15:16 +0530

    added the caching

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,12 +1,23 @@
 import {NextResponse } from "next/server";
 import { dbConnection } from "@/dbConfig/Config";
 import Post from "@/model/Post";
+import { unstable_cache } from "next/cache";
 
+
+const getPosts=unstable_cache(
+    async()=>{
+        await dbConnection();
+        const posts=await Post.find().sort({createdAt :-1}).lean();
+        return JSON.parse(JSON.stringify(posts));
+
+    },
+    ["all-posts"],
+    {revalidate: 60}
+);
 
 export async function GET(){
     try{
-        await dbConnection();
-        const posts=await Post.find().sort({createdAt :-1});
+        const posts=await getPosts();
         if(posts.length==0) return NextResponse.json({error:["No blogs are found create some"]},{status:400});
         // if found then return it
         return NextResponse.json({data:posts},{status:200});
